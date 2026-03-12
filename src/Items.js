cc0fd98b10e6efe6ff801bbf6348ee02cf07099f
JimKhalil <abi.ansyah19052003@gmail.com>
2026-03-12 22:32:11 +0700

    add Items.js

---
+++ src/Items.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: "Spicy Ramen Tokyo",
+    price: 45000,
+    image: "https://images.unsplash.com/photo-1569718212165-3a8278d5f624?w=500",
+    category: "Main",
+  },
+  {
+    id: 2,
+    name: "Creamy Carbonara",
+    price: 52000,
+    image: "https://images.unsplash.com/photo-1612874742237-6526221588e3?w=500",
+    category: "Pasta",
+  },
+  {
+    id: 3,
+    name: "Berry Smoothies",
+    price: 28000,
+    image: "https://images.unsplash.com/photo-1553530666-ba11a7da3888?w=500",
+    category: "Drink",
+  },
+];
--- src/pages/FoodMenu.jsx
+++ src/pages/FoodMenu.jsx
@@ -3,17 +3,14 @@ import { Search } from 'lucide-react';
 import CategoryBar from '../components/CategoryBar';
 import MenuList from '../components/MenuList';
 import CartSheet from '../components/CartSheet';
+import Items from '../Items.js';
 
 const FoodMenu = () => {
   const [cart, setCart] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('All');
   const categories = ['All', 'Main', 'Pasta', 'Drink', 'Snack'];
   
-  const menuItems = [
-    { id: 1, name: 'Spicy Ramen Tokyo', price: 45000, image: 'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?w=500', category: 'Main' },
-    { id: 2, name: 'Creamy Carbonara', price: 52000, image: 'https://images.unsplash.com/photo-1612874742237-6526221588e3?w=500', category: 'Pasta' },
-    { id: 3, name: 'Berry Smoothies', price: 28000, image: 'https://images.unsplash.com/photo-1553530666-ba11a7da3888?w=500', category: 'Drink' },
-  ];
+  const menuItems = Items;
 
   const addToCart = (item) => {
     setCart(prev => {
