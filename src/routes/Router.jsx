0f53a90489f875079de051a26494bb0497f99071
Excel951 <abigailexcelsisdeo@gmail.com>
2026-03-26 13:39:03 +0700

    add register page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,8 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import FoodMenu from "../pages/FoodMenu";
+import SignUpPage from "../pages/SignUpPage.jsx";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <LoginPage /> },
-  { path: "/FoodMenu", element: <FoodMenu /> },
+    {path: "/", element: <LoginPage/>},
+    {path: "/FoodMenu", element: <FoodMenu/>},
+    {path: "/register", element: <SignUpPage/>},
 ]);
